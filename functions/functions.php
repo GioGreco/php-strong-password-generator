cafc63831792fd3ec34bff1640a51eccbe0b5626
GioGreco <grecogiorgio.97@gmail.com>
2022-11-29 16:24:56 +0100

    basic psw generator

--- functions/functions.php
+++ functions/functions.php
@@ -1,3 +1,19 @@
 <?php
+    function generatePSW($pswLEN){
+        $characters = [
+            'alphabet' => 'abcdefghijklmnopqrstuvwxyz',
+            'numbers' => '0123456789',
+            'symbols' => '|$%&/()[]{}?!*#@;<>:'
+        ];
+
+        $result = '';
+        while(strlen($result) < $pswLEN){
+            $key = array_rand($characters);
+            $randCat = $characters[$key];
+            $randChar = $randCat[rand(0, strlen($randCat) - 1)];
+            $result .= $randChar ;
+        }
+        return $result;
+    }
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    
-
+    include __DIR__.'/functions/functions.php';
+    if(isset($_GET['pswLen'])){
+        $newPSW = generatePSW($_GET['pswLen']);
+    }
 ?>
 
 
@@ -14,4 +16,19 @@
         <title>PHP - psw generator</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
+    <body>
+        
+        <div class="vh-100 bg-black d-flex">
+            <div class="left-section d-flex justify-content-center align-items-center w-25">
+                <form class="d-flex flex-column justify-content-around align-items-center" action="index.php" method="GET" name="pswLength">
+                    <input type="number" name="pswLen" id="pswLen">
+                    <button type="submit" class="btn btn-primary">INVIA</button>
+                </form>
+            </div>
+            <div class="right-section d-flex justify-content-center align-items-center text-white w-75">
+                <h2><?php echo $newPSW ?></h2>
+            </div>
+        </div>
+
+    </body>
 </html>
